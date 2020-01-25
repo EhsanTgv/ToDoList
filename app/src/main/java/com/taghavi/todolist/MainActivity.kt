7bc8e176ac44dc78ef1d8866dbe842c45bef74e1
Ehsan Taghavi <EhsanTgv@gmail.com>
2020-01-25 23:37:42 +0330

    Firebase Authenticate & Firebase UI Added Login with email is implemented

--- app/src/main/java/com/taghavi/todolist/MainActivity.kt
+++ app/src/main/java/com/taghavi/todolist/MainActivity.kt
@@ -1,12 +1,56 @@
 package com.taghavi.todolist
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.firebase.ui.auth.AuthUI
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    private val LOG_TAG = MainActivity::class.java.simpleName
+
+    companion object {
+        private const val RC_SIGN_IN = 123
+    }
+
+    private val providers = arrayListOf(
+        AuthUI.IdpConfig.EmailBuilder().build()
+    )
+
+    private var userId: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initFirebase()
+    }
+
+    private fun initFirebase() {
+        userId = FirebaseAuth.getInstance().uid
+        Log.i(LOG_TAG, "userId: $userId")
+
+
+        if (userId == null) {
+            startActivityForResult(
+                AuthUI.getInstance()
+                    .createSignInIntentBuilder()
+                    .setAvailableProviders(providers)
+                    .build()
+                , RC_SIGN_IN
+            )
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN && resultCode == Activity.RESULT_OK) {
+            userId = FirebaseAuth.getInstance().uid
+            Log.i(LOG_TAG, "New userId: $userId")
+        }
     }
 }
